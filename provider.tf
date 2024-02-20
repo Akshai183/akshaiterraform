07431f76d8c010ff026d5e5af29ab784e4d33955
Akshai <akshay05pakhanati@gmail.com>
2024-02-21 01:43:58 +0530

    vnet added

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 # Configure the AWS Provider
 provider "azurerm" {
   features {}
-  #client_id       = "1b0dcb03-c34f-493c-bd23-d916d4437dde"
-  #subscription_id = "e98eb152-e123-49f1-ae2e-7fce29b5faa5"
-  #tenant_id       = "3c959c49-498d-4998-88dd-0eb867ac1a68"
-  #client_secret   = "-jw8Q~35ghC3mCOdpUnu2OmEFFVKzRXoVPzCRbHF"
 }
 
