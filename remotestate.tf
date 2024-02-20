bbd174e74ca83ae878a6e62c76fb92265671e741
Akshai <akshay05pakhanati@gmail.com>
2024-02-21 02:21:21 +0530

    Added Subnets

--- remotestate.tf
+++ remotestate.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-      resource_group_name  = "terraformstate"
-      storage_account_name = "akshaifreestate"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
+    resource_group_name  = "terraformstate"
+    storage_account_name = "akshaifreestate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
