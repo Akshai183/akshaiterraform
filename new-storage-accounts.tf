b38fee113c38681cc5eaaa748aa30b590b22335d
Akshai <akshay05pakhanati@gmail.com>
2024-02-21 20:18:19 +0530

    added 2 new storage accounts

--- new-storage-accounts.tf
+++ new-storage-accounts.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "akshaistr001" {
-  count                    = 1 # 0,1,2
+  count                    = 3 # 0,1,2
   name                     = "akshaistr001${count.index + 1}"
   resource_group_name      = azurerm_resource_group.akshaiterraform.name
   location                 = azurerm_resource_group.akshaiterraform.location
